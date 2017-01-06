173551a6da2118d7e36ec0b9b16f358210b4f527
Raja Asthana <rajaliving@gmail.com>
2017-01-06 20:33:44 +0530

    Java basic program example

---
+++ JavaByExamples/src/com/chap_1/structure/FirstJavaProgram.java
@@ -0,0 +1,28 @@
+package com.chap_1.structure;
+
+
+/**
+ * <p>This program shows the basic structure of a java program.</p>
+ * 
+ * <p>At this point, there is no need to worry about any of the keyword in this program.</p>
+ * 
+ * Things to learn from the program
+ * 	<ul>
+ * 		<li>The name after 'class' is called classname. In this case the classname is 'FirstJavaProgram'</li>
+ * 		<li>The file should be saved in the name of the classname (FirstJavaProgram.java)</li>
+ * 		<li>public static void main() is the starting point of execution</li>
+ * 		<li>To compile the code, use the command 'javac filename.java' (javac FirstJavaProgram.java)
+ * 		<li>Once the compilation is successful, compiler creates a bytecode file (FirstJavaProgram.class) which is used to execute the program</li>
+ * 		<li>To run the code, use the command 'java filename' (java FirstJavaProgram)
+ * 		<li>The program prints 'My First Java Program' to the console</li>
+ * 	</ul>
+ * @author Raja Asthana
+ * @since 6-Jan-2017
+ */
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("My First Java Program");
+	}
+
+}
